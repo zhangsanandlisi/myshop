aa6699bb74f81f1f6375d2dfd032e17a0e7aaeea
ma <ma@ma.com>
2018-03-15 15:48:22 +0800

    first

---
+++ common/config/main.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'timeZone'=>'PRC',
+    'language'=>'zh-CN',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+    ],
+];
